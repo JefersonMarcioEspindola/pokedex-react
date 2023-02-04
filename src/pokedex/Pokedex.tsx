4ab0435d7c61c8423b77d9309af3ed4506df0b59
Jeferson Márcio Espindola <jefersonmarcio2011@gmail.com>
2023-02-04 12:41:13 -0300

    integração com a PokeApi

--- src/pokedex/Pokedex.tsx
+++ src/pokedex/Pokedex.tsx
@@ -1,24 +1,41 @@
-import React, { useState } from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 
 interface PokedexProps {
     
 }
 
-const pokemonArray: string[] = ["Pikachu", "Mew", "Tortic", "Charmander"]
+interface PokemonListInterface {
+    name: string
+    url: string
+}
 
 const Pokedex: React.FC<PokedexProps> = () => {
-    const [pokemons, setPokemons] = useState<string[]>(pokemonArray);
-    const [selectedPokemon, setselectedPokemon] = useState<string | undefined>(undefined);
-  
+    const [pokemons, setPokemons] = useState<PokemonListInterface[]>([]);
+    const [selectedPokemon, setSelectedPokemon] = useState<PokemonListInterface | undefined>(undefined);
+    const [selectedPokemonDetails, setSelectedPokemonDetails] = useState<any | undefined>(undefined);
+ 
+    useEffect(() => {
+        axios.get('https://pokeapi.co/api/v2/pokemon').then((response) => setPokemons(response.data.results))
+    }, []);
+
+    useEffect(() => {
+        if (!selectedPokemon) return;
+
+        axios.get(`https://pokeapi.co/api/v2/pokemon/${selectedPokemon.name}`).then((response) => setSelectedPokemonDetails(response.data))
+        
+    }, [selectedPokemon]);
 
     return (
         <div>
             <h1>Pokedex</h1>
 
             Pokemons:
-            {pokemons.map((pokemon) => <button onClick={() => setselectedPokemon(pokemon)}>{pokemon}</button> )}
+            {pokemons.map((pokemon) => <button onClick={() => setSelectedPokemon(pokemon)}>{pokemon.name}</button>)}
 
-            <h2>Pokemon selecionado: {selectedPokemon || "Nenhum pokemon selecionado"} </h2>
+            <h2>Pokemon selecionado: {selectedPokemon?.name || "Nenhum Pokemon Selecionado"}</h2>
+            {JSON.stringify(selectedPokemonDetails, undefined, 2)}
+
         </div>
     );
 };
